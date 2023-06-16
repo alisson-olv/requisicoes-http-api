a7237dd03680ff0a67715cc27ec97b4e4209a699
Alisson Oliveira <binho_alisson@hotmail.com>
2023-06-15 21:58:08 -0300

    criando fetch de post e get

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,69 @@
+import { useEffect, useState } from "react"
+
+const urlApi = 'http://localhost:3000/products';
 
 function App() {
 
+  const [data, setData] = useState([]);
+
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function getResponse() {
+      const response = await fetch(urlApi);
+      const data = await response.json();
+      setData(data);
+    };
+
+    getResponse();
+  }, []);
+
+  // 2 - adicionando produtos
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const product = {
+      name,
+      price
+    };
+
+    const response = await fetch(urlApi, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(product),
+    });
+  };
+
+
   return (
     <div>
-      Http
+      <h1>Lista de Produtos</h1>
+      <div>
+        <ul>
+          {data && data.map((item) => (
+            <li key={item.id}>
+              <strong>Produto:</strong> {item.name} - R$ {item.price}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name="name" onChange={(event) => setName(event.target.value)} />
+          </label>
+          <label>
+            Preço:
+            <input type="number" value={price} name="price" onChange={(event) => setPrice(event.target.value)} />
+          </label>
+          <input type="submit" value='Criar' />
+        </form>
+      </div>
     </div>
   )
 }
