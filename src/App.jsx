44793b23877662a5c7e3d49fdddc67078978ff16
Alisson Oliveira <binho_alisson@hotmail.com>
2023-06-16 21:06:37 -0300

    atualização de interface e adicionando recurses de post e delete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 
 import { useFetch } from "./hooks/UseFetch";
 
+import { AiFillDelete } from "react-icons/ai";
+
 const urlApi = 'http://localhost:3000/products';
 
 function App() {
@@ -9,18 +11,7 @@ function App() {
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
 
-  const { data: items } = useFetch(urlApi);
-
-  // 1 - resgatando dados
-  // useEffect(() => {
-  //   async function getResponse() {
-  //     const response = await fetch(urlApi);
-  //     const data = await response.json();
-  //     setData(data);
-  //   };
-
-  //   getResponse();
-  // }, []);
+  const { data: items, httpConfig, loading, error } = useFetch(urlApi);
 
   // 2 - adicionando produtos
   const handleSubmit = async (event) => {
@@ -31,37 +22,20 @@ function App() {
       price
     };
 
-    const response = await fetch(urlApi, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(product),
-    });
-
-    // 3 - carregamento dinâmico
-    // const addProducts = await response.json();
-
-    // setData((prevProducts) => [...prevProducts, addProducts]);
+    httpConfig(product, 'POST');
 
     // 4 - resetando inputs
     setName('');
     setPrice('');
   };
 
+  const handleDelete = (id) => {
+    httpConfig(id, 'DELETE');
+  }
 
   return (
     <div>
       <h1>Lista de Produtos</h1>
-      <div>
-        <ul>
-          {items && items.map((item) => (
-            <li key={item.id}>
-              <strong>Produto:</strong> {item.name} - R$ {item.price}
-            </li>
-          ))}
-        </ul>
-      </div>
       <div className="add-product">
         <form onSubmit={handleSubmit}>
           <label>
@@ -72,8 +46,25 @@ function App() {
             Preço:
             <input type="number" value={price} name="price" onChange={(event) => setPrice(event.target.value)} />
           </label>
-          <input type="submit" value='Criar' />
+          {loading && <input type="submit" disabled value='Aguarde' />}
+          {!loading && <input type="submit" value='Criar' />}
         </form>
+      </div>
+      <div>
+        {loading && <p>Carregando dados!</p>}
+        {error && <p>{error}</p>}
+        <ul>
+          {items && items.map((item) => (
+            <li key={item.id}>
+              <strong>Produto:</strong> {item.name} - R$ {item.price}
+              {/* <button onClick={() => handleDelete(item.id)}>Delete</button> */}
+              <AiFillDelete
+                className="icon-delete"
+                onClick={() => handleDelete(item.id)}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
--- src/hooks/UseFetch.jsx
+++ src/hooks/UseFetch.jsx
@@ -3,17 +3,87 @@ import { useEffect, useState } from 'react'
 export const useFetch = (urlApi) => {
   const [data, setData] = useState(null);
 
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  const [error, setError] = useState(null);
+
+  const [itemId, setItemId] = useState(null);
+
+  const httpConfig = (data, method) => {
+    if (method === 'POST') {
+      setConfig({
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      setMethod(method);
+    } else if (method === 'DELETE') {
+      setConfig({
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      setMethod(method);
+      setItemId(data);
+    }
+  }
+
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(urlApi);
-      const json = await response.json();
+      setLoading(true);
 
-      setData(json);
+      try {
+        const response = await fetch(urlApi);
+        const json = await response.json();
+
+        setData(json);
+
+      } catch (error) {
+        console.log(error.message);
+        setError('Houve algum erro, tente novamente mais tarde.');
+      }
+
+      setLoading(false);
     };
 
     fetchData();
+  }, [urlApi, callFetch]);
 
-  }, [urlApi]);
+  // refatorando post
+  useEffect(() => {
+    const httpRequest = async () => {
 
-  return { data };
+      let json
+
+      if (method === 'POST') {
+        let fetchOptions = [urlApi, config];
+
+        const res = await fetch(...fetchOptions);
+        json = await res.json();
+
+      } else if (method === 'DELETE') {
+
+        const deleteUrl = `${urlApi}/${itemId}`;
+        const response = await fetch(deleteUrl, config);
+        json = await response.json();
+
+      };
+      setCallFetch(json);
+    };
+
+    httpRequest();
+  }, [config, method, urlApi]);
+
+  return { data, httpConfig, loading, error };
 };
+
+
